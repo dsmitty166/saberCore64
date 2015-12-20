d12d90b16ea16ed4f57315bd51ab2ebf04a2b081
DespairFactor <matthewdalex92@gmail.com>
2015-12-20 04:05:10 -0500

    out: simplify ramdisk edits

--- out/kernel/edit_ramdisk.sh
+++ out/kernel/edit_ramdisk.sh
@@ -14,64 +14,16 @@ cd /tmp/ramdisk/
 gunzip -c /tmp/ramdisk/boot.img-ramdisk.gz | cpio -i
 cd /
 
-#remove cmdline parameters we do not want
-#maxcpus=2 in this case, which limits smp activation to the first 2 cpus
-echo $(cat /tmp/boot.img-cmdline | sed -e 's/maxcpus=[^ ]\+//')>/tmp/boot.img-cmdline
-
-#add init.d support if not already supported
-#this is no longer needed as the ramdisk now inserts our modules, but we will
-#keep this here for user comfort, since having run-parts init.d support is a
-#good idea anyway.
-found=$(find /tmp/ramdisk/init.rc -type f | xargs grep -oh "run-parts /system/etc/init.d");
-if [ "$found" != 'run-parts /system/etc/init.d' ]; then
-        #find busybox in /system
-        bblocation=$(find /system/ -name 'busybox')
-        if [ -n "$bblocation" ] && [ -e "$bblocation" ] ; then
-                echo "BUSYBOX FOUND!";
-                #strip possible leading '.'
-                bblocation=${bblocation#.};
-        else
-                echo "BUSYBOX NOT FOUND! init.d support will not work without busybox!";
-                echo "Setting busybox location to /system/xbin/busybox! (install it and init.d will work)";
-                #set default location since we couldn't find busybox
-                bblocation="/system/xbin/busybox";
-        fi
-	#append the new lines for this option at the bottom
-        echo "" >> /tmp/ramdisk/init.rc
-        echo "service userinit $bblocation run-parts /system/etc/init.d" >> /tmp/ramdisk/init.rc
-        echo "    oneshot" >> /tmp/ramdisk/init.rc
-        echo "    class late_start" >> /tmp/ramdisk/init.rc
-        echo "    user root" >> /tmp/ramdisk/init.rc
-        echo "    group root" >> /tmp/ramdisk/init.rc
+#Don't force encryption
+if  grep -qr forceencrypt /tmp/ramdisk/fstab.angler; then
+   sed -i "s/forceencrypt/encryptable/" /tmp/ramdisk/fstab.angler
 fi
 
-#copy custom init.shamu.rc
-cp /tmp/fstab.shamu /tmp/ramdisk/fstab.angler
-chmod 750 /tmp/ramdisk/fstab.angler
-
-#remove governor overrides, use kernel default
-#sed -i '/\/sys\/devices\/system\/cpu\/cpufreq\/interactive\/hispeed_freq/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu2\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu3\/cpufreq\/scaling_governor/d' /tmp/ramdisk/init.shamu.power.rc
-#remove min_freq overrides, use kernel default
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_min_freq/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu1\/cpufreq\/scaling_min_freq/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu2\/cpufreq\/scaling_min_freq/d' /tmp/ramdisk/init.shamu.power.rc
-#sed -i '/\/sys\/devices\/system\/cpu\/cpu3\/cpufreq\/scaling_min_freq/d' /tmp/ramdisk/init.shamu.power.rc
-#Gain write access on /system
-if  grep -qr ro.secure=1 /tmp/ramdisk/default.prop; then
- sed -i "s/ro.secure=1/ro.secure=0/" /tmp/ramdisk/default.prop
+#Disable dm_verity
+if  grep -qr verify=/dev/block/platform/msm_sdcc.1/by-name/metadata /tmp/ramdisk/fstab.angler; then
+   sed -i "s/\,verify\=\/dev\/block\/platform\/msm_sdcc\.1\/by\-name\/metadata//" /tmp/ramdisk/fstab.angler
 fi
-
-if  grep -qr verity_load_state /tmp/ramdisk/init.angler.rc; then
- sed -i "s/verity_load_state/#verity_load_state/" /tmp/ramdisk/init.angler.rc
-fi
-if  grep -qr verity_update_state /tmp/ramdisk/init.angler.rc; then
- sed -i "s/verity_update_state/#verity_update_state/" /tmp/ramdisk/init.angler.rc
-fi
-
+rm /tmp/ramdisk/verity_key
 
 rm /tmp/ramdisk/boot.img-ramdisk.gz
 rm /tmp/boot.img-ramdisk.gz
